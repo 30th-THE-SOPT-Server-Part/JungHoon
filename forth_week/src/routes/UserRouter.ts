807a99327bab540717a452de2f4e5ac75fa2d843
jeonghoon <shb03323@naver.com>
2022-05-15 18:12:54 +0900

    level 2 - express-validator 추가

--- forth_week/src/routes/UserRouter.ts
+++ forth_week/src/routes/UserRouter.ts
@@ -1,9 +1,21 @@
 import { Router } from "express";
 import { UserController } from "../controllers";
+import { body } from "express-validator";
 
 const router: Router = Router();
 
-router.post('/', UserController.createUser);
+router.post('/', [
+    body('name')
+        .notEmpty()
+        .isLength({min: 2, max: 8}),
+    body('phone')
+        .notEmpty(),
+    body('email')
+        .isEmail()
+        .normalizeEmail(),
+    body('age')
+        .isInt()
+], UserController.createUser);
 router.put('/:userId', UserController.updateUser);
 router.get('/:userId', UserController.findUserById);
 router.delete('/:userId', UserController.deleteUser);
