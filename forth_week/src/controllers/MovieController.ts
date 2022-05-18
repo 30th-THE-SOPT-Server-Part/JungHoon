8d91b26cb833430e3cc8acaf490c8de323f31fd3
jeonghoon <shb03323@naver.com>
2022-05-18 16:14:32 +0900

    level 2 - export movie service

--- forth_week/src/controllers/MovieController.ts
+++ forth_week/src/controllers/MovieController.ts
@@ -1,4 +1,9 @@
-
+import express, { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import message from "../modules/responseMessage";
+import statusCode from "../modules/statusCode";
+import util from "../modules/util";
+import MovieService from "../services/MovieService";
 
 export default {
 
